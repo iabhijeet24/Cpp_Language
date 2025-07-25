e79515a4ecdac4bf8782b72a394e8dc5949702df
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-07-25 23:02:21 +0530

    basic prblms

--- loops.cpp
+++ loops.cpp
@@ -27,3 +27,19 @@ int main() {
     }
     return 0;
 }
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        cout << "Value of i: " << i << endl;
+    }
+    return 0;
+}
---
+++ rectangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int l , b , area;
+
+    cout << "Enter the length: " << endl;
+    cin >> l;
+
+    cout << "Enter the breadth: " << endl;
+    cin >> b;
+
+    area = l * b;
+    cout << "Area of the rectangle is " << area << endl;
+
+    return 0;
+}
