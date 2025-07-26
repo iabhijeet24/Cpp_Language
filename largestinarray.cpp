6932ed3b4db6f797ea4a5969f763c061506b9024
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-07-26 19:55:27 +0530

    Problems in c++

--- largestinarray.cpp
+++ largestinarray.cpp
@@ -2,27 +2,32 @@
 using namespace std;
 
 int main() {
-    int i, small;
+    int i, small, largest;
     int arr[10];
 
-    cout << "Enter the values in the array: " << endl;
-    for(i=0; i<10; i++)
-    {
-        
+    cout << "Enter the values in the array:" << endl;
+    for(i = 0; i < 10; i++) {
         cin >> arr[i];
     }
 
     small = arr[0];
-
-    for (i = 1; i < 10; i++)
-    {
-        if (arr[i] < small)
-        {
+    for (i = 1; i < 10; i++) {
+        if (arr[i] < small) {
             small = arr[i];
         }
-        
     }
-       cout << "The smallest number in the array is: " << small << endl;
+    
+    largest = arr[0];
+        for (i = 1; i < 10; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    
+
+    cout << "The smallest number in the array is: " << small << endl;
+    cout << "The largest number in the array is: " << largest << endl;
+
     
     return 0;
 }
