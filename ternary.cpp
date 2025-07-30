f6c5f408c7f659fa7fa0fc3804a9158d0b90fd61
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-07-30 08:07:36 +0530

    ternary operator in c++

---
+++ ternary.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a , b ,largest;
+
+    cout << "Enter the first number:" << endl;
+    cin >> a;
+
+    cout << "Enter the second number:" << endl;
+    cin >> b;
+
+    largest = (a>b) ? a : b;
+    cout << a << " is largest." << endl;
+}
